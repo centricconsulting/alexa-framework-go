d3dd2140f24ae752d37af7113986ade3671084a4
William Klos <wjklos@gmail.com>
2016-01-07 22:13:31 -0500

    Initial commit.

---
+++ alexa.go
@@ -0,0 +1,109 @@
+package alexa
+
+//
+type AlexaRequestPacket struct {
+	Version string              `json:"version" xml:"version"`
+	Session AlexaRequestSession `json:"session" xml:"session"`
+	Request json.RawMessage     `json:"request" xml:"request"`
+}
+
+//
+type AlexaRequestSession struct {
+	New         bool              `json:"new" xml:"new"`
+	SessionId   string            `json:"sessionId" xml:"sessionId"`
+	Application AlexaApplication  `json:"application" xml:"application"`
+	Attributes  map[string]string `json:"attributes" xml:"attributes"`
+	User        AlexaUser         `json:"user" xml:"user"`
+}
+
+//
+type AlexaLaunchRequest struct {
+	Type      string `json:"type" xml:"type"`
+	Timestamp string `json:"timestamp" xml:"timestamp"`
+	RequestId string `json:"requestId" xml:"requestId"`
+}
+
+//
+type AlexaApplication struct {
+	ApplicationId string `json:"applicationId" xml:"applicationId"`
+}
+
+//
+type AlexaIntentRequest struct {
+	Type      string      `json:"type" xml:"type"`
+	Timestamp string      `json:"timestamp" xml:"timestamp"`
+	RequestId string      `json:"requestId" xml:"requestId"`
+	Intent    AlexaIntent `json:"intent" xml:"intent"`
+}
+
+//
+type AlexaRequestStub struct {
+	Type string `json:"type" xml:"type"`
+}
+
+//
+type AlexaIntent struct {
+	Name  string                       `json:"name" xml:"name"`
+	Slots map[string]AlexaIntentString `json:"slots" xml:"slots"`
+}
+
+//
+type AlexaIntentString struct {
+	Name  string `json:"name" xml:"name"`
+	Value string `json:"value" xml:"value"`
+}
+
+//
+type AlexaUser struct {
+	UserId string `json:"userId" xml:"userId"`
+}
+
+// A SessionEndedRequest is an object that represents a request made to an Alexa skill to notify that a session was ended.
+type AlexaSessionEndedRequest struct {
+	Type      string `json:"type" xml:"type"`
+	Timestamp string `json:"timestamp" xml:"timestamp"`
+	RequestId string `json:"requestId" xml:"requestId"`
+	Reason    string `json:"reason" xml:"reason"`
+}
+
+// Alexa Response Body Format
+type AlexaResponseBody struct {
+	Version           string            `json:"version" xml:"version"`
+	SessionAttributes map[string]string `json:"sessionAttributes,omitempty" xml:"sessionAttributes"`
+	Response          AlexaResponse     `json:"response" xml:"response"`
+}
+
+// Alexa Response Body Format
+type AlexaResponse struct {
+	OutputSpeech     AlexaResponseOutputSpeech `json:"outputSpeech" xml:"outputSpeech"`
+	Card             AlexaResponseCard         `json:"card,omitempty" xml:"card"`
+	Reprompt         AlexaResponseReprompt     `json:"reprompt,omitempty" xml:"reprompt"`
+	ShouldEndSession bool                      `json:"shouldEndSession" xml:"shouldEndSession"`
+}
+
+// Alexa Response Body Format
+type AlexaResponseCard struct {
+	Type    string `json:"type" xml:"type"`
+	Title   string `json:"title" xml:"title"`
+	Content string `json:"content" xml:"content"`
+} //
+
+// Alexa Response Body Format
+type AlexaResponseReprompt struct {
+	OutputSpeech AlexaResponseOutputSpeech `json:"outputSpeech" xml:"outputSpeech"`
+} //
+
+// Alexa Response Body Format
+type AlexaResponseOutputSpeech struct {
+	Type string `json:"type" xml:"type"`
+	Text string `json:"text" xml:"text"`
+	SSML string `json:"ssml" xml:"ssml"`
+} //
+
+// Alexa template responses structure.
+type AlexaTemplateResponse struct {
+	Name      string   `json:"name"`
+	Purpose   string   `json:"purpose"`
+	Responses []string `json:"responses"`
+	IsActive  bool     `json:"is_active"`
+}
